68a767c515497cf01e1972a992bd0052f2f08700
sajedehsaremi <85531280+sajedehsaremi@users.noreply.github.com>
2021-10-03 18:45:55 -0400

    Update config.js
    Update/Clean API Key

--- static/js/config.js
+++ static/js/config.js
@@ -1,4 +1,4 @@
 // API key
-const API_KEY = "APIT kEY Here";
+const API_KEY = "YOUR API KEY HERE!";
 let earthquakeJsonPath7days = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
 let tectonicURL = 'Data\PB2002_boundaries.json'
